887442589ea72d1dd3446a66f72aad059acfbef7
Lars Koreska Andersen <lkan12@student.aau.dk>
2018-04-11 09:17:05 +0200

    Maracas
    Maracas

--- Assets/Scripts/Maracas.cs
+++ Assets/Scripts/Maracas.cs
@@ -40,18 +40,15 @@ public class Maracas : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         OVRInput.Update();
-        
+
+        Debug.Log(OVRInput.GetLocalControllerVelocity(scriptL.controller).z);
 
         if (scriptL.grabbing == true && scriptL.grabbedObject.name == "maracas")
         {
             transform.localPosition = new Vector3(0.045f, 0.0138f, 0.044f);
 
         }
-        Debug.Log(speed);
-        if (speed != OVRInput.GetLocalControllerVelocity(scriptL.controller))
-        {
-            Debug.Log("ITS WORKING!");
-        }
+
         // If there is an increase og decrease in any xyz by 0.2f play sound. 
         // The problem right now is that float values in vectors is only showing 1 decimal, while there is alot of them. 
        
